20ce606f09ecc5689ef822b06c3eae70d70a67a9
DALyskov <DALyskov@gmail.com>
2020-04-07 21:47:03 +0300

    Выполнил задание 2

---
+++ src/components/list-container.js
@@ -0,0 +1,7 @@
+const createListContainerTemplate = () => {
+  return (
+    `<div class="films-list__container"></div>`
+  );
+};
+
+export {createListContainerTemplate};
---
+++ src/components/show-more-btn.js
@@ -0,0 +1,7 @@
+const createShowMoreBtnTemplate = () => {
+  return (
+    `<button class="films-list__show-more">Show more</button>`
+  );
+};
+
+export {createShowMoreBtnTemplate};
