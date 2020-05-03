d4775d9240ad66f818d9f3af619598433e9ddb14
DALyskov <DALyskov@gmail.com>
2020-05-03 19:05:48 +0300

    Выполнил задание 5.2

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,16 +1,23 @@
 import AbstractComponent from './abstract-component.js';
 
-const sortingNames = [`default`, `date`, `rating`];
+// const sortingNames = [`default`, `date`, `rating`];
+
+export const SortType = {
+  DEFAULT: `default`,
+  DATE: `date`,
+  RATING: `rating`,
+};
 
 const createSortMarkup = (sortType, isActive) => {
   return (
     `<li><a href="#" class="sort__button ${isActive ?
-      `sort__button--active` : ``}">Sort by ${sortType}</a></li>`
+      `sort__button--active` : ``}" data-sort-type='${sortType}'>Sort by ${sortType}</a></li>`
   );
 };
 
 const createSortingTemplate = () => {
-  const sortsMarkup = sortingNames.map((v, i) => createSortMarkup(v, i === 0)).join(`\n`);
+  // const sortsMarkup = sortingNames.map((v, i) => createSortMarkup(v, i === 0)).join(`\n`);
+  const sortsMarkup = Object.values(SortType).map((v, i) => createSortMarkup(v, i === 0)).join(`\n`);
 
   return (
     `<ul class="sort">
@@ -20,8 +27,34 @@ const createSortingTemplate = () => {
 };
 
 export default class Sorting extends AbstractComponent {
+  constructor() {
+    super();
+    this._currentSortType = SortType.DEFAULT;
+  }
 
   getTemplate() {
     return createSortingTemplate();
+  }
+
+  getSortType() {
+    return this._currentSortType;
+  }
+
+  setSortTypeHangeHandler(handler) {
+    this.getElm().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      if (evt.target.tagName !== `A`) {
+        return;
+      }
+
+      const newSortType = evt.target.dataset.sortType;
+
+      if (newSortType === this._currentSortType) {
+        return;
+      }
+
+      this._currentSortType = newSortType;
+      handler(this._currentSortType);
+    });
   }
 }
